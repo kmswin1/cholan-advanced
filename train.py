3dbd1e9c5d632ade49df32a10015ea9045095b69
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-11-16 14:21:29 +0900

    debugging

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ from sentence_transformers import SentenceTransformer
 import torch
 import numpy as np
 from transformers import BertTokenizer, BertModel
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
 np.random.seed(7)
 
 def train():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    bert = BertModel.from_pretrained('bert-base-uncased')
     model = SentenceTransformer('distilbert-base-nli-mean-tokens')
     dataset = Dataset()
     dataloader = DataLoader(dataset, shuffle=True, batch_size=128)
@@ -19,11 +19,14 @@ def train():
         optimizer.zero_grad()
         mention, sentence, gold = batch_data
         negative = np.random.choice(dataset.id2candidate, 1)[0]
-
-        mention_embedding = tokenizer(mention, return_tensors="pt")
-        sentence_embedding = tokenizer(sentence, return_tensors="pt")
-        gold_embedding = tokenizer(gold, return_tensors="pt")
-        negative_embedding = tokenizer(negative, return_tensors="pt")
+        mention_embedding = model.encoder(mention)
+        sentence_embedding = model.encoder(sentence)
+        gold_embedding = model.encoder(gold)
+        negative_embedding = model.encoder(negative)
+        #mention_embedding = tokenizer(mention, return_tensors="pt")
+        #sentence_embedding = tokenizer(sentence, return_tensors="pt")
+        #gold_embedding = tokenizer(gold, return_tensors="pt")
+        #negative_embedding = tokenizer(negative, return_tensors="pt")
 
         pos = torch.sigmoid(torch.dot(mention_embedding, gold_embedding))
         neg = torch.sigmoid(torch.dot(mention_embedding, negative_embedding))
